0ecf48dda8922293a5d807bfbabc4e54f55df368
Sepehr Nikiyan <sepehrnikiyan@gmail.com>
2025-02-08 14:33:57 +0330

    just minifying code a little

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const taskInput = document.getElementById("taskInput");
 const addTaskButton = document.getElementById("addTask");
 const taskList = document.getElementById("taskList");
 
-
 // افزودن کار جدید به لیست
 addTaskButton.addEventListener("click", function () {
     const taskText = taskInput.value.trim(); // حذف فاصله‌های اضافی
